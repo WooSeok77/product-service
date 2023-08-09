fc52bd689456787763d3d8caa13d7a16f695156a
WooSeok77 <wi8tkf@naver.com>
2023-08-09 17:51:00 +0900

    feat: BasicProductController 구현

---
+++ src/main/java/hello/productservice/web/basic/BasicProductController.java
@@ -0,0 +1,38 @@
+package hello.productservice.web.basic;
+
+import hello.productservice.domain.product.Product;
+import hello.productservice.domain.product.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
+
+@Controller
+@RequestMapping("/basic/products")
+@RequiredArgsConstructor
+public class BasicProductController {
+    private final ProductRepository productRepository;
+
+    @GetMapping
+    public String products(Model model) {
+        List<Product> products = productRepository.findAll();
+        model.addAttribute("products", products);
+        return "basic/products";
+    }
+
+    /**
+     * 테스트용 데이터 추가
+     */
+    @PostConstruct
+    public void init() {
+        productRepository.save(new Product("productA", 10000, 10));
+        productRepository.save(new Product("productB", 20000, 20));
+
+    }
+
+}
