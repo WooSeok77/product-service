2862c89d484e3ca62f3fb4600be1ef03381e41ba
WooSeok77 <wi8tkf@naver.com>
2023-08-10 14:22:29 +0900

    feat: 제품상세조회 구현 및 템플릿 작성

--- src/main/java/hello/productservice/web/basic/BasicProductController.java
+++ src/main/java/hello/productservice/web/basic/BasicProductController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.annotation.PostConstruct;
@@ -33,6 +34,13 @@ public class BasicProductController {
         productRepository.save(new Product("productA", 10000, 10));
         productRepository.save(new Product("productB", 20000, 20));
 
+    }
+
+    @GetMapping("/{productId}")
+    public String product(@PathVariable Long productId, Model model) {
+        Product product = productRepository.findById(productId);
+        model.addAttribute("product", product);
+        return "basic/product";
     }
 
 }
