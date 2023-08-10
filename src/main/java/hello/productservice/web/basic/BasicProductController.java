bbdec96ab7255591ad4fc6225a022d2fc54dbc04
WooSeok77 <wi8tkf@naver.com>
2023-08-10 17:08:28 +0900

    feat:리다이렉트관련 구현

--- src/main/java/hello/productservice/web/basic/BasicProductController.java
+++ src/main/java/hello/productservice/web/basic/BasicProductController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
@@ -47,9 +48,11 @@ public class BasicProductController {
     }
 
     @PostMapping("/add")
-    public String addProduct(Product product) {
-        productRepository.save(product);
-        return "basic/product";
+    public String addProduct(Product product, RedirectAttributes redirectAttributes) {
+        Product savedproduct = productRepository.save(product); 
+        redirectAttributes.addAttribute("productId", savedproduct.getId()); 
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/basic/products/{productId}";
     }
 
     @GetMapping("/{productId}/edit")
